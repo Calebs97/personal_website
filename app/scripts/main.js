3fc24f92b7b76e26f04d4ba09df479677874242a
Caleb Siu <calebs11@berkeley.edu>
2017-07-04 21:15:28 -0700

    Website initial progress

---
+++ app/scripts/main.js
@@ -0,0 +1,40 @@
+$(function() {
+	$('.parallax').parallax();
+
+	$('.project-btn').click(function() {
+		$('html, body').animate({
+			scrollTop: $('#project').offset().top
+		}, 1000, 'swing');
+	});
+	$('.intro-btn').click(function() {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000, 'swing');
+	});
+
+	$('#intro h1').addClass('animated fadeInUp');
+	setTimeout(function(){
+		$('#intro h4').addClass('animated fadeInUp');
+	}, 300);
+	setTimeout(function() {
+		$('#intro a').addClass('animated zoomIn');
+		$('#intro a').css('opacity', 1);
+	}, 800);
+
+	var options = [];
+	for (var i = 1; i <= 9; i++) {
+		var elementClass = '.col' + i;
+
+		$(elementClass).attr('delay', 200 * ((i - 1) % 3));
+
+		var option = {
+			selector: elementClass, offset: 100, callback: function(element) {
+				setTimeout(function() {
+					$(element).addClass('animated fadeInUp');
+				}, $(element).attr('delay'));
+			}
+		}
+		options.push(option);
+	}
+	Materialize.scrollFire(options);
+});
---
+++ app/scripts/project.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+  $(".post-module").hover(function() {
+    $(this).find(".description").stop().animate(
+      {
+        height: "toggle",
+        opacity: "toggle"
+      },
+      400
+    );
+  });
+});
